a7ecfd1af1d2b339fa1f35fd083c1778d3322739
Beef Liu <beef.liu@gmail.com>
2023-07-28 15:17:15 +0800

    commons-fileupload  1.2.2 -> 1.5

--- backupOldSrcBeforeChangeToMaven/src/SalamaCloudDataService/src/com/salama/service/clouddata/defaultsupport/DefaultSupportService.java
+++
@@ -1,9 +0,0 @@
-package com.salama.service.clouddata.defaultsupport;
-
-public class DefaultSupportService {
-
-	public final static String version() {
-		return "2015.10.14";
-	}
-	
-}
--- backupOldSrcBeforeChangeToMaven/src/SalamaUtil/src/com/salama/util/expression/IValueStack.java
+++
@@ -1,5 +0,0 @@
-package com.salama.util.expression;
-
-public interface IValueStack {
-	Object findValue(String name);
-}
